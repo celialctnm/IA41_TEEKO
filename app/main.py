b2f76fc1b5abc90127ba7ecb0f5dfa072507e1ff
celialctnm <celia.leichtnam@gmail.com>
2022-11-20 15:42:44 +0100

    fonction pour pouvoir cliquer les labels + amélioration pour la fonction dessinerGrille

--- app/main.py
+++ app/main.py
@@ -7,5 +7,8 @@ import controller
 
 modele.dessinerPlateau()
 controller.quatrePremierTour()
+
+# test fonction labelClickable
+modele.labelClickable()
 # laisser cette ligne pour ne pas que la fenêtre se ferme au bout des 8 placements vu que le code est pas fini
 modele.fenetre.mainloop()
--- app/modele.py
+++ app/modele.py
@@ -1,3 +1,4 @@
+import numpy
 import numpy as np
 from tkinter import *
 
@@ -6,11 +7,11 @@ fenetre = Tk()
 
 # création du plateau de jeu
 grille = [
-    [0,0,0,0,0],
-    [0,0,0,0,0],
-    [0,0,0,0,0],
-    [0,0,0,0,0],
-    [0,0,0,0,0]
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0]
 ]
 
 # initialisation des pions
@@ -18,15 +19,61 @@ pionJ1 = PhotoImage(file="bleu.png")
 pionJ2 = PhotoImage(file="orange.png")
 caseVide = PhotoImage(file="vide.png")
 
+# liste labels
+labels = []
+for i in range(25):
+    labels.append(Label)
+
+# liste label cliqué
+touchPress = []
+
+
 # affichage graphique du plateau en fonction des éléments du tableau 2d grille
 def dessinerPlateau():
+    cpt = 0
     for ligne in range(5):
         for colonne in range(5):
             if grille[ligne][colonne] == 1:
-                Label(fenetre, image=pionJ1, borderwidth=1).grid(row=ligne, column=colonne)
+                J1Lab = Label(fenetre, cursor="plus", image=pionJ1, borderwidth=1)
+                J1Lab.grid(row=ligne, column=colonne)
+                labels[cpt] = J1Lab
+                cpt += 1
+                # labels.append(Label(fenetre, image=pionJ1, borderwidth=1))
+                # Label(fenetre, image=pionJ1, borderwidth=1).grid(row=ligne, column=colonne)
             elif grille[ligne][colonne] == 2:
-                Label(fenetre,image=pionJ2, fg='white', borderwidth=1).grid(row=ligne, column=colonne)
+                J2Lab = Label(fenetre, cursor="plus", image=pionJ2, fg='white', borderwidth=1)
+                J2Lab.grid(row=ligne, column=colonne)
+                labels[cpt] = J2Lab
+                cpt += 1
             else:
-                Label(fenetre, image=caseVide, borderwidth=1).grid(row=ligne, column=colonne)
-            # mise à jour de la fenêtre
-            fenetre.update()
+                J3Lab = Label(fenetre, image=caseVide, borderwidth=1)
+                J3Lab.grid(row=ligne, column=colonne)
+                labels[cpt] = J3Lab
+                cpt += 1
+    # mise à jour de la fenêtre
+    # print(labels)
+    fenetre.update()
+
+
+# réalise une action lorsqu'un label est cliqué
+def mouseClick(event):
+    print("mouse clicked")
+
+    # ajouter les labels cliqués dans une liste
+    touchPress.append(event.widget)
+
+    pion1 = Label
+    caseVide = Label
+
+    # si deux labels sont cliqués, ils devront être interveti, le pion prendrait la place de la case vide
+    if len(touchPress) == 2:
+        pion1 = touchPress[0]
+        caseVide = touchPress[1]
+        # fonction pour déplacer les pions en fonction des deux cases selectionnées
+        touchPress.clear()
+
+
+# rend les labels clickables à l'aide la souris
+def labelClickable():
+    for label in labels:
+        label.bind("<Button-1>", mouseClick)
